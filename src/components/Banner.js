c0d4340301fb4cca3cff155efe6974f20df6683b
SagarTeotia1 <147220406+SagarTeotia1@users.noreply.github.com>
2024-09-09 20:57:32 +0530

    commit --icons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,8 +11,8 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
-  const period = 2000;
+  const toRotate = [ "Society" ];
+  const period = 200;
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -55,8 +55,8 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                {/* <span className="tagline">Welcome to StartupSphere!</span> */}
+                <h1>{`Welcome to StartupSphere `} <span className="txt-rotate" dataPeriod="200" data-rotate='[ "a Society", "a Community" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,9 @@
-import meter1 from "../assets/img/meter1.svg";
+import EM from "../assets/icons/Event.png";
 import meter2 from "../assets/img/meter2.svg";
+import Tech from "../assets/icons/tech.png";
+import SM from "../assets/icons/social-media.png";
+import photo from "../assets/icons/photo.png";
+// import Tech from "../assets/icons/tech.png";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
@@ -34,24 +38,57 @@ export const Skills = () => {
             <div className="row">
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <h2>Departments</h2>
+                        <p>Departments in Startupsphere organize events, offer mentorship, promote brands, develop tech, and create revenue strategies, working together to drive startup growth and success.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={EM} alt="Image" />
+                                <h5>Event
+                                Management</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Startup
+                                Development</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Content
+                                Creation</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={SM} alt="Image" />
+                                <h5>Social Media
+                                Management</h5>
+                            </div>
+                            <div className="item">
+                                <img src={EM} alt="Image" />
+                                <h5>Monitization
+                                Strategies</h5>
+                            </div>
+                            <div className="item">
+                                <img src={EM} alt="Image" />
+                                <h5>Public
+                                Relations</h5>
+                            </div>
+                            <div className="item">
+                                <img src={EM} alt="Image" />
+                                <h5>Research &
+                                Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={EM} alt="Image" />
+                                <h5>Marketing</h5>
+                            </div>
+                            <div className="item">
+                                <img src={Tech} alt="Image" />
+                                <h5>Tech &
+                                Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={EM} alt="Image" />
+                                <h5>Photography &
+                                Videography</h5>
                             </div>
                         </Carousel>
                     </div>
