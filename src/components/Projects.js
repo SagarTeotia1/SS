f0284ba9487ad152242761ed866329d5592a1987
Abhinav Mishra <itsabhinav2005@gmail.com>
2024-09-10 02:07:01 +0530

    Final

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,65 +1,84 @@
+
 import React from 'react';
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
+import projImg7 from "../assets/img/project-img7.png"; // New
+import projImg8 from "../assets/img/project-img8.png"; // New
+import projImg9 from "../assets/img/project-img9.png"; // New
+import projImg10 from "../assets/img/project-img10.png"; // New
+import projImg11 from "../assets/img/project-img11.png"; // New
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
-  const webProjects = [       //webinars
+  const webProjects = [       //webinar
     {
-      title: "Symposium",
-      description: "Rotaract Club Event",
-      imgUrl: projImg1,
+      title: "STARTUP AND MARKETING",
+      description: "By Guari Joshi",
+      imgUrl: projImg10,
     },
     {
-      title: "Fish Tank",
-      description: "Startup Pitch Competition",
-      imgUrl: projImg2,
+      title: "HOW TO BUILD A STARTUP",
+      description: "By Sarthak Mittal",
+      imgUrl: projImg5,
     },
     {
-      title: "Round Table Conference",
-      description: "Startup Ideas Event",
-      imgUrl: projImg3,
+      title: "MARKETING & STARTUP", // New project
+      description: "By Sarah Nitin Rawat",
+      imgUrl: projImg6,
     },
   ];
 
   const mobileProjects = [ // Competitions
     {
-      title: "Mobile App 1",
-      description: "Innovative Mobile Solution",
-      imgUrl: projImg2,
+        title: "Symposium",
+        description: "Rotaract Club Event",
+        imgUrl: projImg1,
+      },
+      {
+        title: "Fish Tank",
+        description: "Startup Pitch Competition",
+        imgUrl: projImg2,
+      },
+      {
+        title: "Round Table Conference",
+        description: "Startup Ideas Event",
+        imgUrl: projImg3,
+      },
+    {
+      title: "INVEST FUSION",
+      description: "Trading Competition",
+      imgUrl: projImg4,
     },
     {
-      title: "Mobile App 2",
-      description: "User-Friendly Interface",
-      imgUrl: projImg3,
+      title: "INTERNSHIP FAIR",
+      description: "Symposium : Socio-Economic Enterpreneurial Competition",
+      imgUrl: projImg7,
     },
     {
-      title: "Mobile App 3",
-      description: "Cross-Platform Development",
-      imgUrl: projImg1,
+      title: "CASE STUDY COMPETITION", // New project
+      description: "Innovate , Compete & Excel",
+      imgUrl: projImg8,
     },
   ];
 
   const ecommerceProjects = [       //Summits
     {
-      title: "E-commerce Platform",
-      description: "Online Retail Solution",
-      imgUrl: projImg3,
+      title: "SIH BOOTCAMP - JIIT", // New project
+      description: "Pichathon Organised By AICTC",
+      imgUrl: projImg9,
     },
     {
-      title: "Payment Gateway",
-      description: "Secure Transaction System",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Inventory Management",
-      description: "Efficient Stock Control",
-      imgUrl: projImg2,
+      title: "Indian Institute Of Technology Delhi", // New project
+      description: "Society Summit Organised By IITD",
+      imgUrl: projImg11,
     },
   ];
 
@@ -79,10 +98,10 @@ export const Projects = () => {
                       <Nav.Link eventKey="first">Webinars</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Competition</Nav.Link>
+                      <Nav.Link eventKey="second">Competitions</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">summits</Nav.Link>
+                      <Nav.Link eventKey="third">Summits</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
